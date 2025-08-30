5b538628cbfae08c31939124c31b3a7005a53171
Yuvaraaj17 <yuvaraaj5910@gmail.com>
2025-08-30 20:19:01 +0530

    Sliding menu bar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,10 @@
 'use client'
-import React from 'react'
+import React, { useContext } from 'react'
 import { IoMdClose } from "react-icons/io";
 import sections from "../src/assets/data/sections.json";
 import { IoMenu } from "react-icons/io5";
 import { LuSunMoon } from "react-icons/lu";
+import { ThemeContext } from '../context/ThemeContext';
 
 export default function NavBar(props : {isMenuBarOpen: boolean, toggleMenuBar: () => void }) {
 
@@ -16,18 +17,25 @@ export default function NavBar(props : {isMenuBarOpen: boolean, toggleMenuBar: (
     props.toggleMenuBar();
   }
 
+  const { theme , setTheme } = useContext(ThemeContext);
+
   return (
-    <div className='flex flex-row justify-between p-3 gap-3 lg:gap-6 lg:border-b border-violet-300 '>
-      <div className='bg-blue-400 w-10 h-10 flex items-center justify-center'>L</div>
-      <div className='hidden lg:flex flex-row gap-32 bg-black items-center justify-end w-full text-2xl font-mono pr-5'>
-        {sections.map((section: sectionType, key) => <div key={key}>{section.title}</div>)}
+    <div className='flex flex-row justify-between p-3 gap-3 lg:gap-6 border-b-2 border-amber-600'>
+      <div className='bg-blue-400 w-10 h-10 flex items-center justify-center'>L {theme}</div>
+      <div className={`${theme === 'dark' ? 'text-white' : 'text-black'} hidden lg:flex flex-row gap-32 items-center font-bold justify-end w-full text-2xl font-mono pr-5 hover:cursor-pointer`}>
+        {sections.map((section: sectionType, key) => <div key={key} className='hover:text-amber-500 transition-all ease-in-out text-3xl'>{section.title}</div>)}
       </div>
       <div className='flex flex-row gap-4 items-center lg:pr-5'>
-        <LuSunMoon className='text-xl lg:text-3xl'/>
-        <IoMenu className='lg:hidden' size={30} onClick={handleClick} />
+        <LuSunMoon className={`${theme === 'dark' ? 'text-white' : 'text-amber-600'} text-xl  lg:text-3xl`} onClick={()=>setTheme( theme === 'dark' ? 'light' : 'dark')}/>
+        <IoMenu className={`${theme === 'dark' ? 'text-white' : 'text-amber-600'} lg:hidden`} size={30} onClick={handleClick} />
       </div>
       {props.isMenuBarOpen && (
-        <div className='h-full bg-white text-black absolute lg:hidden right-0 w-52 md:w-2/5 p-3 gap-3 flex flex-col'>
+        <div
+          className="fixed inset-0 bg-black/50 z-40"
+          onClick={() => props.toggleMenuBar()} // closes when clicking background
+        />
+      )}
+        <div className={`fixed top-0 right-0 shadow-lg z-50 transform transition-transform duration-300 h-full bg-white text-black lg:hidden w-52 md:w-2/5 p-3 gap-3 flex flex-col ${props.isMenuBarOpen ? "translate-x-0" : "translate-x-full"}`}>
           <div className='border-b-2 border-black p-2 flex justify-end w-full'>
             <IoMdClose onClick={handleClick} />
           </div>
@@ -35,7 +43,6 @@ export default function NavBar(props : {isMenuBarOpen: boolean, toggleMenuBar: (
             {sections.map((section: sectionType, key) => <div key={key}>{section.title}</div>)}
           </div>
         </div>
-      )}
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client'
-import React from "react";
+import React, { useContext } from "react";
 import NavBar from "../../components/NavBar";
-import { useState } from "react";
+import { useState, createContext } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
+
 export default function Page() {
 
   const [isOpen, setIsOpen] = useState(false);
+  const { theme , setTheme } = useContext(ThemeContext);
 
   const toggleMenuBar = () => {
     console.log('clicked parent')
@@ -12,10 +15,11 @@ export default function Page() {
   }
 
   return (
-    <div className="bg-black text-white h-full w-full flex flex-col">
-      <NavBar isMenuBarOpen={isOpen} toggleMenuBar={toggleMenuBar}/>
-      <div>Hello world</div>
-    </div>
+      <div className={`${theme === 'dark' ? 'bg-black' : 'bg-white'} text-white h-full w-full flex flex-col`}>
+        <NavBar isMenuBarOpen={isOpen} toggleMenuBar={toggleMenuBar} />
+        <div>Hello world</div>
+      </div>
+    
     // <div className="bg-red-400 flex flex-col border border-black gap-28">
       // {/* 
       // <Intro />
