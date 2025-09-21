e154f36d9d061a35996ecfab2dfccb875b72b760
Yuvaraaj17 <yuvaraaj5910@gmail.com>
2025-09-21 19:51:36 +0530

    Contact submission with supabase

--- src/app/api/contactSubmission/route.ts
+++ src/app/api/contactSubmission/route.ts
@@ -4,8 +4,9 @@ import { supabase } from "../../../../utils/client";
 export async function POST(request: NextRequest) {
   try {
     const { emailId, name, message } = await request.json();
-    const result = await supabase
-      .from("contact") // Replace 'posts' with your table name
+
+    const { data, error } = await supabase
+      .from("contact")
       .insert([
         {
           name: name,
@@ -14,15 +15,17 @@ export async function POST(request: NextRequest) {
         },
       ]);
 
-    // If there's an error during insertion, return error response
-    if (result) {
-      return NextResponse.json({ error: result.error.message }, { status: 500 });
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
     }
-    return NextResponse.json({ message: "Submitted successfully" }, { status: 200 });
-  } catch {
-    // Catch any other errors
+
     return NextResponse.json(
-      { error: "An unexpected error occurred" },
+      { message: "Submitted successfully", data },
+      { status: 200 }
+    );
+  } catch (err) {
+    return NextResponse.json(
+      { error: `An unexpected error occurred ${err}` },
       { status: 500 }
     );
   }
